d6b5aab84e8745a0a4f5de5b5b8b2302a5df4b55
Ken Withee <ken.withee@microsoft.com>
2017-09-20 14:28:11 -0700

    add title prog and readme

---
+++ add_meta_title.py
@@ -0,0 +1,24 @@
+import os
+
+path_in = r'c:\users\kenwith\git\office-docs-powershell\skype\skype-ps\skype/'
+path_out = r'c:\processed/'
+
+
+
+for file in os.listdir(path_in):
+    f_in = open(os.path.join(path_in, file), 'r', encoding='utf8')
+    f_out = open(os.path.join(path_out, file), 'w', encoding='utf8')
+
+    cmdlet_name, file_extension = os.path.splitext(file)
+
+
+    lines = f_in.readlines()
+
+    lines.insert(3, "title: " + cmdlet_name + "\n")
+
+    for line in lines:
+        f_out.write(line) 
+
+    f_in.close()
+    f_out.close()
+
