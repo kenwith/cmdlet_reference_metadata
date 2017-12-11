a63b8e7babe4d78b5d217dd880d9705605261daf
Ken Withee <kenwith@microsoft.com>
2017-12-11 14:02:33 -0800

    add script to update TOC, add_cmdlets_to_toc.py

---
+++ add_cmdlets_to_toc.py
@@ -0,0 +1,16 @@
+import os
+
+f_in = open("fast_cmdlets", 'r', encoding='utf8')
+f_out = open("fast_cmdlets_processed", 'w', encoding='utf8')
+
+lines = f_in.readlines()
+
+for line in lines:
+    cmdlet = line.rstrip()
+    cmdlet_with_md = cmdlet + ".md"
+    print("PROCESSED: %s" % cmdlet)
+    f_out.write(r"### [" + cmdlet + r"](" + cmdlet_with_md + r")" + "\n")
+    f_out.write(r"{{Manually Enter " + cmdlet + r" Description Here}}" + "\n\n")
+
+f_in.close()
+f_out.close()
